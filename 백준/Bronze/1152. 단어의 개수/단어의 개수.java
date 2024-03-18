d1b3fa412cdec47a779a43a421af8355a8d5a48e
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2024-03-18 13:58:50 +0900

    [Bronze II] Title: 단어의 개수, Time: 272 ms, Memory: 20048 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,34 @@
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ * 영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 이 문자열에는 몇 개의 단어가 있을까? 이를 구하는 프로그램을 작성하시오.
+ * 단, 한 단어가 여러 번 등장하면 등장한 횟수만큼 모두 세어야 한다.
+ *
+ * 입력
+ * 첫 줄에 영어 대소문자와 공백으로 이루어진 문자열이 주어진다. 이 문자열의 길이는 1,000,000을 넘지 않는다.
+ * 단어는 공백 한 개로 구분되며, 공백이 연속해서 나오는 경우는 없다. 또한 문자열은 공백으로 시작하거나 끝날 수 있다.
+ *
+ * AA
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+         //단어 개수
+        StringTokenizer str = new StringTokenizer(bf.readLine(), " ");
+        int count = str.countTokens();
+        System.out.println(count);
+
+
+
+
+    }
+}
