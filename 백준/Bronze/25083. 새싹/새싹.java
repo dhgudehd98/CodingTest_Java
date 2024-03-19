3f716642f70f470298e9aed3109e3eced221d202
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2024-03-19 11:26:30 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 14060 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,30 @@
+
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+import static javax.print.attribute.standard.MediaSizeName.A;
+import static javax.print.attribute.standard.MediaSizeName.B;
+
+/**
+ * 
+ */
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+
+
+
+    }
+}
