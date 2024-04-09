9a10bda47b1e215c1fbe74259ca46fc13c635c9b
dhgudehd98 <97084827+dhgudehd98@users.noreply.github.com>
2024-04-09 11:19:09 +0900

    [Bronze V] Title: A×B, Time: 204 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,22 @@
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/**
+ *
+ */
+public class Main
+{
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int  a= scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.println(a * b);
+     
+    }
+}
